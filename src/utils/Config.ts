8da628605e69016eac22a3b7b7434dd049f2a73e
jiejunh <jiejunh@hotmail.com>
2021-06-24 15:57:24 +0800

    first commit

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,6 +1,6 @@
 export const Config = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Settle',
+  title: 'Nextjs Settle',
+  description: 'Settle your real estate needs',
   locale: 'en',
 };
